59be7069a95bde307469a82767bb95a39b2eed8c
Anaflavia <anaflaviadmar@gmail.com>
2025-03-07 23:02:49 +0100

    feat: add checkout route

---
+++ src/app/features/checkout/checkout.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+const checkoutRoutes: Routes = [
+  { path: '', loadComponent: () => import('./checkout.component') },
+];
+
+export default checkoutRoutes;
